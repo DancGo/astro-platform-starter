49ced2362002635a04d9b611515d80f4be0ddc9d
DanceGo <40678370+DancGo@users.noreply.github.com>
2025-04-26 15:19:33 +0800

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,5 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
@@ -7,3 +8,14 @@ export default defineStackbitConfig({
     "contentSources": [],
     "postInstallCommand": "npm i --no-save @stackbit/types"
 })
+
+export default {
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+    })
+  ]
+};
