1ba979711801bd2cbab3cb1fe53cfac1257dbedd
DanceGo <40678370+DancGo@users.noreply.github.com>
2025-04-28 11:51:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import { ContentfulContentSource } from "@stackbit/cms-contentful";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,14 +10,4 @@ export default defineConfig({
     },
     integrations: [react()],
     adapter: netlify(),
-
-    //stackbit
-    contentSources: [
-        new ContentfulContentSource({
-          spaceId: process.env.CONTENTFUL_SPACE_ID,
-          environment: process.env.CONTENTFUL_ENVIRONMENT,
-          previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
-          accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
-        })
-      ]
 });
